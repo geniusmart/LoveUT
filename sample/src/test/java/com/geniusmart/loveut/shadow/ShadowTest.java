7f7ad1359217fffe7f9c102e9dfd0033088c30c3
geniusmart <270391484@qq.com>
2016-01-26 10:20:19 +0800

    some UT sample

--- sample/src/test/java/com/geniusmart/loveut/shadow/ShadowTest.java
+++ sample/src/test/java/com/geniusmart/loveut/shadow/ShadowTest.java
@@ -1,12 +1,24 @@
 package com.geniusmart.loveut.shadow;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
 import com.geniusmart.loveut.BuildConfig;
+import com.geniusmart.loveut.activity.MainActivity;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.Shadows;
 import org.robolectric.annotation.Config;
 import org.robolectric.internal.ShadowExtractor;
+import org.robolectric.shadows.ShadowActivity;
+import org.robolectric.shadows.ShadowApplication;
+import org.robolectric.shadows.ShadowBitmap;
 
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertNull;
 import static junit.framework.TestCase.assertEquals;
 
 @RunWith(CustomShadowTestRunner.class)
@@ -17,7 +29,7 @@ public class ShadowTest {
      * 测试自定义的Shadow
      */
     @Test
-    public void testCustom(){
+    public void testCustomShadow(){
         Person person = new Person("genius");
         //getName()实际上调用的是ShadowPerson的方法
         assertEquals("geniusmart", person.getName());
@@ -27,8 +39,25 @@ public class ShadowTest {
         assertEquals("geniusmart", shadowPerson.getName());
     }
 
+    /**
+     * 测试框架提供的Shadow类
+     */
     @Test
-    public void testDefault(){
+    public void testDefaultShadow(){
+
+        MainActivity mainActivity = Robolectric.setupActivity(MainActivity.class);
+
+        //通过Shadows.shadowOf()可以获取很多Android对象的Shadow对象
+        ShadowActivity shadowActivity = Shadows.shadowOf(mainActivity);
+        ShadowApplication shadowApplication = Shadows.shadowOf(RuntimeEnvironment.application);
+
+        Bitmap bitmap = BitmapFactory.decodeFile("Path");
+        ShadowBitmap shadowBitmap = Shadows.shadowOf(bitmap);
+
+        //Shadow对象提供方便我们用于模拟业务场景进行测试的api
+        assertNull(shadowActivity.getNextStartedActivity());
+        assertNull(shadowApplication.getNextStartedActivity());
+        assertNotNull(shadowBitmap);
 
     }
 
