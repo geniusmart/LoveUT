3ecea851e27190f19f747706184ec98c77b98960
geniusmart <270391484@qq.com>
2016-03-03 22:02:36 +0800

    commit

--- app/src/main/java/com/geniusmart/loveut/model/GithubService.java
+++ app/src/main/java/com/geniusmart/loveut/model/GithubService.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 import retrofit2.http.Url;
